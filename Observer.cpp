ae5f6252faf78d6a7d77256db2097f73a3f2d43e
PRO pre 4.BI, 4.CI a 4.AT <knmspsit36@gmail.com>
2025-10-15 20:44:30 +0200

    Update Observer.cpp

--- Observer.cpp
+++ Observer.cpp
@@ -2,4 +2,4 @@
 // Created by Adéla Martynková on 09.10.2025.
 //
 
-#include "Observer.h"
+#include "Observer.h" // vymazať nie je to nutne clion vytvorí .h and .cpp nakolko je tu absencia interface stači .h 
